075dbc88353943920a9d529d2a6aae5b6dac49bd
Xandarel <aobordug@gmail.com>
2020-11-11 14:38:17 +0500

    Исправленны замечания. Логика промокодов вынесена из сущности корзины( ну или я пытался)

--- BookStore/Program.cs
+++ BookStore/Program.cs
@@ -10,82 +10,104 @@ namespace BookStore
     class ShoppingCart
     {
         private List<IBook> books;
+        private List<IBook> ebooks;
+        private List<IBook> paperBook;
+        private List<IPromo> codes;
         private decimal priceSumm;
         private decimal deliver;
         private decimal deliverSumm;
         public ShoppingCart()
         {
             deliver = 200;
+            codes = new List<IPromo>();
             books = new List<IBook>();
+            ebooks = new List<IBook>();
+            paperBook = new List<IBook>();
             priceSumm = 0;
             deliverSumm = 0;
         }
-        public decimal GetCheck() => priceSumm + deliver;
+        public decimal GetCheck() 
+        {
+            foreach (var book in books)
+            {
+                priceSumm += book.Price;
+                if (book.Type == BookType.paperBook)
+                    deliverSumm += book.Price;
+                if (deliverSumm > 1000)
+                    deliver = 0;
+                // Куплены только электронные книги. Доставка не нужна.
+                if (deliverSumm == 0)
+                    deliver = 0;
+            }
+            CheckEvent();
+            foreach (var promo in codes)
+                ActivatePromo(promo);
+            return priceSumm + deliver;
+        }
 
         public void AddBook(IBook book)
         {
             books.Add(book);
-            priceSumm += book.Price;
-            if (book.Type == BookType.paperBook)
-                deliverSumm += book.Price;
-            if (deliverSumm > 1000)
-                deliver = 0;
+            switch (book.Type)
+            {
+                case BookType.paperBook:
+                    paperBook.Add(book);
+                    break;
+                case BookType.eBook:
+                    ebooks.Add(book);
+                    break;
+            }
         }
 
         public void AddPromo(IPromo promo)
         {
-            var find_element = books.Contains(promo.Book);
-            if (find_element)
+            codes.Add(promo);
+        }
+        private void ActivatePromo(IPromo promo)
+        {
+            try
             {
-                var PromoSumm = promo.CalculateSale();
-                if (promo.PromoType == PromoType.FreeDelivery)
-                    deliver = 0;
+                var find_element = books.Contains(promo.Book);
+                if (find_element)
+                    priceSumm -= promo.CalculateSale();
                 else
-                    priceSumm -= PromoSumm;
+                    throw new ArgumentException("Этой книги нет в вашей корзине");
             }
-            else
-                throw new ArgumentException("Этой книги нет в вашей корзине");
+            catch
+            {
+                //если find_element выдаст ошибку - значит это промокод на доставку
+                deliver = 0;
+            }
         }
 
         public void CheckEvent()
         {
             var authorsBook = new Dictionary<string, int>();
             var eBookAuthor = new Dictionary<string, List<IBook>>();
-            foreach(var book in books)
+            foreach (var paper in paperBook )
             {
-                if (book.Type != BookType.paperBook)
-                {
-                    if (authorsBook[book.Author] > 1)
-                    {
-                        if (!eBookAuthor.ContainsKey(book.Author))
-                        {
-                            eBookAuthor[book.Author] = new List<IBook>
-                            {
-                                book
-                            };
-                        }
-                        else
-                            eBookAuthor[book.Author].Add(book);
-                    }
-
-                }
+                if (!authorsBook.ContainsKey(paper.Author))
+                    authorsBook[paper.Author] = 1;
                 else
-                {
-                    if (!authorsBook.ContainsKey(book.Author))
+                    authorsBook[paper.Author] += 1;
+            }
+            foreach (var ebook in ebooks)
+            {
+                if (authorsBook.ContainsKey(ebook.Author) && (authorsBook[ebook.Author] > 1))
+                    if (!eBookAuthor.ContainsKey(ebook.Author))
                     {
-                        authorsBook[book.Author] = 1;
+                        eBookAuthor[ebook.Author] = new List<IBook>
+                        {
+                            ebook
+                        };
                     }
                     else
-                        authorsBook[book.Author] += 1;
-                }
+                        eBookAuthor[ebook.Author].Add(ebook);
             }
-            foreach(var author in authorsBook.Keys)
+            foreach(var author in eBookAuthor.Keys)
             {
-                if (authorsBook[author] >= 2)
-                {
-                    Console.WriteLine("Вы купили две бумажные книги автора {0}. в подарок вам полагается электронная книга этого автора", author);
-                    priceSumm -= eBookAuthor[author].Min(a => a.Price);
-                }
+                Console.WriteLine("Вы купили две бумажные книги автора {0}. в подарок вам полагается электронная книга этого автора", author);
+                priceSumm -= eBookAuthor[author].Min(a => a.Price);
             }
         }
     }
